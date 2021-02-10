4cb3e5dc81cb80f7e882a8bb1f50d0d888e56e36
Daniel Armas <daniel62armas@gmail.com>
2021-02-09 21:01:48 -0600

    Using XMLHttp to consume an API with callbacks

---
+++ src/callback/star-wars-challenge.js
@@ -0,0 +1,23 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API = 'https://swapi.dev/api/people/';
+
+const fetchData = (uri, callback) =>{
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', uri, true);//->Para conectarse a la API
+    xhttp.onreadystatechange = event => {
+        if(xhttp.readyState === 4){
+            if(xhttp.status === 200){
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error(`Chale salio mal ${uri}`);
+                return callback(error, null);
+            }
+        }
+    }
+    xhttp.send();
+}
+//Callbacks
+fetchData(API, (error1, data1) => {
+    if(error1) return console.error(error1);
+    console.log(data1.results[0].url);
+});
